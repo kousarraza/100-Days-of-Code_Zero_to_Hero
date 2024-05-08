c425c3c5f02182bf8110fe483bf08d33ee02fa8a
KousarRaza <kousarraza69@gmail.com>
2024-05-08 20:59:21 +0500

    Day_18

---
+++ Day_18/Q_185/Q_185.py
@@ -0,0 +1,18 @@
+#  Write a  Python program to demonstrating various dictionary methods.
+# Dictionary methods
+my_dict = {'apple': 5, 'banana': 3, 'orange': 7}
+
+# Length of dictionary
+print(len(my_dict))
+
+# Check if a key exists
+print('apple' in my_dict)
+
+# Get value for a key
+print(my_dict.get('banana'))
+
+# Remove all items
+my_dict.clear()
+
+# Printing the empty dictionary
+print(my_dict)
