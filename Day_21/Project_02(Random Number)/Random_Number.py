c88266022e9de31a040c2d9f34dd506fe5859be3
KousarRaza <kousarraza69@gmail.com>
2024-05-12 12:57:47 +0500

    ..

---
+++ Day_21/Project_02(Random Number)/Random_Number.py
@@ -0,0 +1,17 @@
+""" from random import randint
+L=[]
+for i in range(20):
+   print() L.append(randint(1,101)))
+print(x) """
+
+""" from random import randint
+L = []
+for i in range(50):
+    print(L.append(randint(1,100)))
+
+ """
+import random
+
+# Generate 10 random numbers between 1 and 100 (inclusive)
+for _ in range(10):
+  print(random.randint(1, 100))
