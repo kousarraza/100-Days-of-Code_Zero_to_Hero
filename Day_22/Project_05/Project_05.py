837cbef06f1af4414344bcf1ca14170518e9a558
KousarRaza <kousarraza69@gmail.com>
2024-05-14 16:14:04 +0500

    .

--- Day_22/Project_05/Project_05.py
+++ Day_22/Project_05/Project_05.py
@@ -1,9 +1,16 @@
 from random import randint
+# Generate a random number between 1 and 100 (inclusive)
 secret_num = randint(1,100)
+
+# Initialize turns and guess variables
 num_guesses = 0
 guess = 0
+# Game loop 
 while guess != secret_num and num_guesses <= 4:
+
+    # Ask the question
     guess = eval(input('Enter your guess (1-100): '))
+    # Check guess
     num_guesses = num_guesses + 1
     if guess < secret_num:
         print('HIGHER.', 5-num_guesses, 'guesses left.\n')
@@ -11,5 +18,6 @@ while guess != secret_num and num_guesses <= 4:
         print('LOWER.', 5-num_guesses, 'guesses left.\n')
     else:
          print('You got it!')
+ #  check the answer 
 if num_guesses==5 and guess != secret_num:
     print('You lose. The correct number is', secret_num)
